881ca3adbed00d6d49ee13bd1e3926a6760a614f
griffin568 <messenger133@laposte.net>
2019-05-27 11:23:55 +0200

    maj

--- src/view/MapFrame.java
+++ src/view/MapFrame.java
@@ -2,6 +2,41 @@ package view;
 import java.awt.*;
 import javax.swing.*;
 
-public class MapFrame extends JFrame {
+public class MapFrame extends JPanel {
+
+  private TopBar top;
+  private JPanel left;
+  private JTextField depart;
+  private JTextField arrive;
+  private JTextArea info;
+  private JEditorPane map;
+
+  /**
+    *Create a new MapFrame object
+    */
+    public MapFrame() {
+      initComponent();
+    }
+
+  /**
+    *Initialize the components
+    */
+    public void initComponent() {
+      this.top = new TopBar();
+      this.left = new JPanel();
+      this.depart = new JTextField("Adresse de départ");
+      this.arrive = new JTextField("Adresse d'arrivée");
+      this.info = new JTextArea();
+      this.map = new JEditorPane();
+
+      this.left.setLayout(new BorderLayout());
+      JPanel upperLeft = new JPanel(new GridLayout(2,1));
+      JPanel d = new JPanel(new GridLayout(1,2));
+      d.add(this.depart);
+      d.add(new ImageIcon("pin.png"));
+      upperLeft.add(d);
+      upperLeft.add(this.info);
+      add(upperLeft, BorderLayout.WEST);
+    }
 
 }
