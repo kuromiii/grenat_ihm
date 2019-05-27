0496233efdd5f125a496345282cb01b968726105
Drmarsupial35 <you@example.com@gmail.com>
2019-05-27 11:21:43 +0200

    PNG images

--- src/view/TopBar.java
+++ src/view/TopBar.java
@@ -3,9 +3,11 @@ import java.awt.*;
 import javax.swing.*;
 
 public class TopBar extends JPanel {
-
+  ImageIcon logo;
+  Jpanel barreLiens;
 
   public TopBar() {
-    
+    this.logo = new ImageIcon("../img/logo.png");
+    this.barreLiens = new JPanel();
   }
 }
