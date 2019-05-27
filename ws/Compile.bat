2c4609f856e516a638fdc248080a83b79ddeaad9
clAwz <dominator56600@gmail.com>
2019-05-27 10:54:46 +0200

    Removed leftover from previous assignment

--- ws/Compile.bat
+++ ws/Compile.bat
@@ -1,9 +1,7 @@
 echo off
 cls
 title Compiler
-REM color 0A
 javac -d ../class ../src/*.java
 pause
 javadoc -encoding utf8 -docencoding utf8 -charset utf8 -private -d ../doc ../src/*.java
-java TestTableau
 pause
