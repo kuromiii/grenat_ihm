93dcb296328e1d53965c4c96a4d672b00df1b489
Drmarsupial35 <you@example.com@gmail.com>
2019-05-27 11:05:25 +0200

    Added the logo and create TopBar class

---
+++ src/view/TopBar.java
@@ -0,0 +1,11 @@
+package view;
+import java.awt.*;
+import javax.swing.*;
+
+public class TopBar extends JPanel {
+
+
+  public TopBar() {
+    
+  }
+}
---
+++ src/view/VehicleFrame.java
@@ -0,0 +1,7 @@
+package view;
+import java.awt.*;
+import javax.swing.*;
+
+public class VehicleFrame extends JFrame {
+
+}
