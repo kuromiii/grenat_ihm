e69f78efba9789c997af2bcc29c102d9061847b6
Drmarsupial35 <you@example.com@gmail.com>
2019-05-27 12:40:40 +0200

    New Images

--- src/view/VehicleFrame.java
+++ src/view/VehicleFrame.java
@@ -2,6 +2,14 @@ package view;
 import java.awt.*;
 import javax.swing.*;
 
-public class VehicleFrame extends JFrame {
+public class VehicleFrame extends JPanel {
 
+
+  public VehicleFrame() {
+    JTextField msg = new JTextField("Bonjour\nQuel moyen de transport voulez-vous utiliser aujourd'hui ?");
+    JPanel buttons = new JPanel();
+
+    JButton bus = new JButton()
+    buttons.add(new ImageIcon("../img/Bus.png"));
+  }
 }
