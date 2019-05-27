b9185f737416ecaa1c0714e7798b6daa88b32698
Drmarsupial35 <you@example.com@gmail.com>
2019-05-27 11:27:21 +0200

    TopBar

--- src/view/TopBar.java
+++ src/view/TopBar.java
@@ -4,10 +4,18 @@ import javax.swing.*;
 
 public class TopBar extends JPanel {
   ImageIcon logo;
-  Jpanel barreLiens;
+  JPanel barreLiens;
+  JButton Itinéraire;
+  JButton Fonctionnel;
+  JButton Agenda;
+  JButton Parametres;
 
   public TopBar() {
     this.logo = new ImageIcon("../img/logo.png");
     this.barreLiens = new JPanel();
+    this.Itinéraire = new JButton("Itinéraire");
+    this.Fonctionnel = new JButton("Fonctionnel");
+    this.Agenda = new JButton("Agenda");
+    this.Parametres = new JButton("Parametres");
   }
 }
