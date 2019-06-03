e145515dbe31478354236579be8e361895f3d706
griffin568 <messenger133@laposte.net>
2019-06-03 14:31:14 +0200

    maj

--- src/view/MainMenuFrame.java
+++ src/view/MainMenuFrame.java
@@ -6,10 +6,10 @@ public class MainMenuFrame extends JPanel {
 
   private JButton boutonItineraire;
   private JButton boutonAgenda;
-  private JButton boutonFonctionnel;
+  private JButton boutonTransport;
   private JButton boutonItineraireTexte;
   private JButton boutonAgendaTexte;
-  private JButton boutonFonctionnelTexte;
+  private JButton boutonTransportTexte;
 
   /**
     *Creates a new MainMenuFrame object
@@ -24,14 +24,27 @@ public class MainMenuFrame extends JPanel {
     private void initComponent() {
       boutonItineraire = new JButton();
       boutonAgenda = new JButton();
-      boutonFonctionnel = new JButton();
+      boutonTransport = new JButton();
       boutonItineraireTexte = new JButton("Itineraire");
       boutonAgendaTexte = new JButton ("Agenda");
-      boutonFonctionnelTexte = new JButton("Fonctionnel");
+      boutonTransportTexte = new JButton("Transports");
       setLayout(new BorderLayout());
-      add(new TopBar(),BorderLayout.NORTH);
       JPanel centralPanel = new JPanel(new GridLayout(1,3));
       JPanel itineraire = new JPanel(new BorderLayout());
-    //  boutonItineraire.setIcon("../img/")
+      boutonItineraire.setIcon("../img/Itineraire.png");
+      itineraire.add(boutonItineraire,BorderLayout.CENTER);
+      itineraire.add(boutonItineraire,BorderLayout.SOUTH);
+      centralPanel.add(itineraire);
+      JPanel agenda = new JPanel(new BorderLayout());
+      boutonAgenda.setIcon("../img/Agenda.png");
+      agenda.add(boutonAgenda);
+      agenda.add(boutonAgendaTexte);
+      centralPanel.add(agenda);
+      JPanel transports = new JPanel (new BorderLayout());
+      boutonTransport.setIcon("../img/Transport.png");
+      transports.add(boutonTransport);
+      transports.add(boutonTransportTexte);
+      centralPanel.add(transports);
+      add(transports,BorderLayout.CENTER);
     }
 }
