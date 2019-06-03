bd73816d6c1897df4c3a9ec74d01ff7aab47011b
griffin568 <messenger133@laposte.net>
2019-06-03 14:35:25 +0200

    maj

--- src/view/MainMenuFrame.java
+++ src/view/MainMenuFrame.java
@@ -28,20 +28,31 @@ public class MainMenuFrame extends JPanel {
       boutonItineraireTexte = new JButton("Itineraire");
       boutonAgendaTexte = new JButton ("Agenda");
       boutonTransportTexte = new JButton("Transports");
+
+
+      Image img = (new ImageIcon("../img/Itinéraire.png")).getImage();
+      Image newImg = img.getScaledInstance(30,30,Image.SCALE_SMOOTH);
+      boutonItineraire.setIcon(new ImageIcon(newImg));
+      Image img2 = (new ImageIcon("../img/Agenda.png")).getImage();
+      Image newImg2 = img2.getScaledInstance(30,30,Image.SCALE_SMOOTH);
+      boutonAgenda.setIcon(new ImageIcon(newImg2));
+      Image img3 = (new ImageIcon("../img/Transport.png")).getImage();
+      Image newImg3 = img.getScaledInstance(30,30,Image.SCALE_SMOOTH);
+      boutonTransport.setIcon(new ImageIcon(newImg3));
+
+
       setLayout(new BorderLayout());
+
       JPanel centralPanel = new JPanel(new GridLayout(1,3));
       JPanel itineraire = new JPanel(new BorderLayout());
-      boutonItineraire.setIcon("../img/Itineraire.png");
       itineraire.add(boutonItineraire,BorderLayout.CENTER);
       itineraire.add(boutonItineraire,BorderLayout.SOUTH);
       centralPanel.add(itineraire);
       JPanel agenda = new JPanel(new BorderLayout());
-      boutonAgenda.setIcon("../img/Agenda.png");
       agenda.add(boutonAgenda);
       agenda.add(boutonAgendaTexte);
       centralPanel.add(agenda);
       JPanel transports = new JPanel (new BorderLayout());
-      boutonTransport.setIcon("../img/Transport.png");
       transports.add(boutonTransport);
       transports.add(boutonTransportTexte);
       centralPanel.add(transports);
