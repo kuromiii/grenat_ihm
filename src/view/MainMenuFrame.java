ef0796beefb470a1f084ffe47ef2664518af4f30
griffin568 <messenger133@laposte.net>
2019-06-03 15:24:00 +0200

    maj

--- src/view/MainMenuFrame.java
+++ src/view/MainMenuFrame.java
@@ -40,44 +40,46 @@ public class MainMenuFrame extends JPanel {
       Image newImg3 = img3.getScaledInstance(120,120,Image.SCALE_SMOOTH);
       boutonTransport.setIcon(new ImageIcon(newImg3));
 
+
+
       setLayout(new BorderLayout());
 
-      JPanel centralPanel = new JPanel(new GridLayout(1,3));
+      JPanel centralPanel = new JPanel(new GridLayout(1,5));
       JPanel itineraire = new JPanel(new BorderLayout());
       itineraire.add(boutonItineraire,BorderLayout.CENTER);
       itineraire.add(boutonItineraireTexte,BorderLayout.SOUTH);
 
-      JPanel itineraireContainer = new JPanel(new GridLayout(5,1));
-      itineraireContainer.add(new JPanel());
+      JPanel itineraireContainer = new JPanel(new GridLayout(3,1));
       itineraireContainer.add(new JPanel());
       itineraireContainer.add(itineraire);
       itineraireContainer.add(new JPanel());
-      itineraireContainer.add(new JPanel());
       centralPanel.add(itineraireContainer);
+      centralPanel.add(new JLabel(""));
 
       JPanel agenda = new JPanel(new BorderLayout());
       agenda.add(boutonAgenda,BorderLayout.CENTER);
       agenda.add(boutonAgendaTexte,BorderLayout.SOUTH);
-
-      JPanel agendaContainer = new JPanel(new GridLayout(5,1));
-      agendaContainer.add(new JPanel());
+      JPanel agendaContainer = new JPanel(new GridLayout(3,1));
       agendaContainer.add(new JPanel());
       agendaContainer.add(agenda);
       agendaContainer.add(new JPanel());
-      agendaContainer.add(new JPanel());
       centralPanel.add(agendaContainer);
+      centralPanel.add(new JLabel(""));
 
       JPanel transports = new JPanel (new BorderLayout());
       transports.add(boutonTransport,BorderLayout.CENTER);
       transports.add(boutonTransportTexte,BorderLayout.SOUTH);
 
-      JPanel transportsContainer = new JPanel(new GridLayout(5,1));
-      transportsContainer.add(new JPanel());
+      JPanel transportsContainer = new JPanel(new GridLayout(3,1));
       transportsContainer.add(new JPanel());
       transportsContainer.add(transports);
       transportsContainer.add(new JPanel());
-      transportsContainer.add(new JPanel());
       centralPanel.add(transportsContainer);
-      add(centralPanel,BorderLayout.CENTER);
+
+      JPanel centralContainer = new JPanel(new BorderLayout());
+      centralContainer.add(new JLabel(" "),BorderLayout.WEST);
+      centralContainer.add(new JLabel(" "),BorderLayout.EAST);
+      centralContainer.add(centralPanel,BorderLayout.CENTER);
+      add(centralContainer,BorderLayout.CENTER);
     }
 }
