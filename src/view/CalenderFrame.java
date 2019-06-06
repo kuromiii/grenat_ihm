fb30a4aca15442a17f1663400b82b7fca2fb88e7
griffin568 <messenger133@laposte.net>
2019-06-06 08:28:37 +0200

    maj

--- src/view/CalenderFrame.java
+++ src/view/CalenderFrame.java
@@ -2,11 +2,15 @@ package view;
 
 import java.awt.*;
 import javax.swing.*;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Date;
 
 public class CalenderFrame extends JPanel {
 
   private JButton addTrajet;
-  private JPanel Calendar;
+  private JPanel calendrier;
+  private GregorianCalendar cal;
 
   public CalenderFrame() {
     try {
@@ -24,6 +28,18 @@ public class CalenderFrame extends JPanel {
 
   public void initComponents() {
     addTrajet = new JButton("Ajouter un trajet");
+    calendrier = new JPanel(new GridLayout(11,8))
+    cal = new GregorianCalendar();
+    setLayout(new BorderLayout());
+
+    JPanel down = new JPanel(new GridLayout(1,3));
+    down.add(new JLabel());
+    down.add(new JLabel());
+    down.add(addTrajet);
+
+    JPanel up = new JPanel (new GridLayout(1,8));
+    int week = cal.get(Calendar.WEEK_OF_YEAR);
+
 
   }
 }
